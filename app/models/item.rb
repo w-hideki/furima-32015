d21df8b5490dda3c0046ba9cc32d5a5dc9b61a8e
w-hideki <73641870+w-hideki@users.noreply.github.com>
2020-11-19 17:28:38 +0900

    taxの計算js実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,15 +12,14 @@ class Item < ApplicationRecord
 
 
   with_options presence: true do
-    
-    validates :product_name
-    validates :product_detail
-    validates :category_id, numericality: { other_than: 1 } 
-    validates :product_condition_id
-    validates :delivery_fee_id
-    validates :area_id
-    validates :days_to_ship_id
-    validates :price
-    validates :image
+     validates :product_name, :product_detail, :image
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id, :product_condition_id, :delivery_fee_id, :area_id, :days_to_ship_id 
+    end
+      
+    with_options format: { with: /\A[0-9]+\z/ } do
+      validates :price, numericality: { greater_than: 300, less_than: 9999999 }
+    end
   end
 end
