61d9cce4873404e4032ee3e377a8d749d3eaf8d7
w-hideki <73641870+w-hideki@users.noreply.github.com>
2020-11-20 09:06:34 +0900

    first_commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  # before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index]
   # before_action :move_to_index, except: [:index]
   def index
     # @items = Item.includes(:user)
