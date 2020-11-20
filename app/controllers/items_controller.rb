9ec2e1a5e29441c032818a1c365f3d02dd67d415
w-hideki <73641870+w-hideki@users.noreply.github.com>
2020-11-20 16:46:56 +0900

    レビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,9 +41,9 @@ class ItemsController < ApplicationController
 
   def destroy
     if user_signed_in? && current_user.id == @item.user_id
-      @item.destroy
-      redirect_to root_path
+      @item.destroy.valid?
     end
+       redirect_to root_path
   end
 
   private
