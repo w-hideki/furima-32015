518cfe6a7ecb6021f780a730851f8c70bbdcca9d
w-hideki <73641870+w-hideki@users.noreply.github.com>
2020-11-23 08:06:12 +0900

    軽微修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   before_action :set_order, only: [:index, :create]
 
   def index
-    redirect_to root_path if current_user.id == @item.user_id || !@item.order.nil?
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
     @order_address = OrderAddress.new
   end
 
